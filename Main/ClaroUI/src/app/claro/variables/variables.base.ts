8566fb6e7e610aac901ad7a85ae8cf1ae2c9a12e
slondono_acc <stiven.londono@accenture.com>
2017-08-18 15:05:14 -0500

    publilcacion

--- Main/ClaroUI/src/app/claro/variables/variables.base.ts
+++ Main/ClaroUI/src/app/claro/variables/variables.base.ts
@@ -1,4 +1,4 @@
 export var environment = {
-    API_BASE_URL: "http://localhost:58831/"
-    // API_BASE_URL: "http://clarogncservice.azurewebsites.net/"
+    //API_BASE_URL: "http://localhost:58831/"
+    API_BASE_URL: "http://clarogncservice.azurewebsites.net/"
 }
